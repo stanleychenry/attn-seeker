f960aef925e2677f519b8c9821654363a24847e2
Stanley Henry <stanley@theattentionseeker.com>
2026-03-05 10:53:30 +1300

    refactor: update EVENT_DATE constant for ERC webinar to reflect new date

--- src/app/webinar/ERC/constants.ts
+++ src/app/webinar/ERC/constants.ts
@@ -3,6 +3,6 @@
 export const STRIPE_PAYMENT_URL =
   "https://buy.stripe.com/7sY9AV0QX4mk6zh5u11ck0a?utm_source=meta&utm_medium=paid&utm_campaign=erc-webinar-anz&utm_content=landing-page";
 
-export const EVENT_DATE = "March 5";
+export const EVENT_DATE = "March 26";
 
 export const IMAGE_BASE = "/Webinar Landing Page Photos";
